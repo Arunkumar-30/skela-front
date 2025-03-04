1afcc795a814ea8d62d826e4f2937beceb5bdc34
Arunkumar-30 <Selvam.arun100@gmail.com>
2025-03-04 18:56:28 +0530

    dd

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -1,5 +1,13 @@
 'use client';
 
+import Image from "next/image";
+const members = [
+  { id: 1, src: '/banner/image-1.png', name: 'Member 1' },
+  { id: 2, src: '/banner/image-2.png', name: 'Member 2' },
+  { id: 3, src: '/banner/image-3.png', name: 'Member 3' },
+  { id: 4, src: '/banner/image-4.png', name: 'Member 4' },
+  { id: 5, src: '/banner/image-5.png', name: 'Member 5' },
+];
 const BannerComponent = ()=>{
     return(
         <>
@@ -9,11 +17,27 @@ const BannerComponent = ()=>{
                <h1>Your Global 
                Dream Starts Here</h1>
             </div>
-            <div className=" circle">
-
+            <div className="circle">
+            <div>
+                 <div className="circle-img">
+                     <Image src={'/banner/group-1.png'} width={272} height={358.15020751953125} alt="circle-img"/>
+                 </div>
+                 <div className="circle-alt ">
+                  {
+                    members.map((data=>(
+                      <>
+                        <Image src={data.src} alt={data.name} width={64} height={64}/>
+                      </>
+                    )))
+                  }
+                 </div>
+           
             </div>
+            </div>
+         
 
           </div>
+       
         </div>
         </>
     )
